7db8d0c9ce6db717c7f682dde93451c1d9290ac2
dianamnaranjo92 <dianamariand92@gmail.com>
2020-07-07 11:57:29 +0200

    editing script

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 #Create env.js file
+cd /testing-jpl-sqaaas
 echo 'module.exports = {
     provider_url: "https://aai-dev.egi.eu",
     client_id: "10333f08-7e8f-4cc4-bba0-e66f4e5505d0",        
