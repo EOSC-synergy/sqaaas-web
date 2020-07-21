0d796fd3e6ff7b29074d613aace6379bdadddaa2
Samuel Bernardo <samuel@lip.pt>
2020-07-21 20:38:41 +0100

    Changes to use Dockerfile approach, improving pipeline run time
    Signed-off-by: Samuel Bernardo <samuel@lip.pt>

---
+++ Dockerfile
@@ -0,0 +1,11 @@
+FROM node
+
+#Install ghp-import
+RUN apt-get -y update && apt-get -y install ghp-import python-pip
+RUN pip install setuptools
+
+#Install dependencies
+RUN npm install
+
+#Create static website
+npm run build
--- script2.sh
+++ script2.sh
@@ -3,30 +3,21 @@ cd /testing-jpl-sqaaas
 
 #Install ghp-import
 
-apt-get update
-apt-get -y install ghp-import
-apt-get -y install python-pip
-pip install setuptools 
+#apt-get update
+#apt-get -y install ghp-import
+#apt-get -y install python-pip
+#pip install setuptools
 git branch -a
 #Clone repository
 #git clone https://github.com/EOSC-synergy/sqaaas-web.git
 #cd sqaaas-web
 #git branch -a
 
-
-echo 'module.exports = {
-    provider_url: "https://aai-dev.egi.eu",
-    client_id: "10333f08-7e8f-4cc4-bba0-e66f4e5505d0",        
-    redirect_uri: "https://eosc-synergy.github.io/sqaaas-web/callback",
-    url_authorize: "https://aai-dev.egi.eu/oidc/authorize",
-    url_user_info: "https://aai-dev.egi.eu/oidc/userinfo",
-}' > src/env.js
-
 #Install dependencies
-npm install
+#npm install
 
 #Create static website
-npm run build
+#npm run build
 
 git config --global user.email "dianamariand92@gmail.com"
 git config --global user.name "dianamariand92"
