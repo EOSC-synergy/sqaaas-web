78c8d63584868c3bdf41faea12bc47028ce6c1cd
dianamnaranjo92 <dianamariand92@gmail.com>
2020-07-21 10:17:16 +0200

    editing script file

--- script.sh
+++ script.sh
@@ -1,8 +1,18 @@
 cd /testing-jpl-sqaaas
 #Create env.js file
 
-git clone https://$GITHUB_TOKEN@github.com/EOSC-synergy/sqaaas-web.git
+#Install ghp-import
+
+apt-get update
+apt-get -y install ghp-import
+apt-get -y install python-pip
+pip install setuptools 
+git branch -a
+#Clone repository
+git clone https://github.com/EOSC-synergy/sqaaas-web.git
 cd sqaaas-web
+git branch -a
+
 
 echo 'module.exports = {
     provider_url: "https://aai-dev.egi.eu",
@@ -20,14 +30,7 @@ npm run build
 
 git config --global user.email "dianamariand92@gmail.com"
 git config --global user.name "dianamariand92"
+git config credential.helper '!f() { sleep 1; echo "username=${GIT_USER}"; echo "password=${GIT_PASSWORD}"; }; f'
 
-#Go to the dist folder of the gh-pages branch
-git checkout -b gh-pages origin/gh-pages
-cd dist/
-cp -R * ../
-cd ..
+ghp-import -p dist/
 
-#Push changes to gh-pages branch
-git add *
-git commit -m "Updating web in gh-pages branch"
-git push -u origin gh-pages
