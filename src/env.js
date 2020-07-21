037f3451aff4c1f65b20caf3e81ea3ac0cbf2428
dianamariand92 <dianamariand92@gmail.com>
2020-07-21 08:09:04 +0000

    Update documentation

--- src/env.js
+++
@@ -1,7 +0,0 @@
-module.exports = {
-    provider_url: "https://aai-dev.egi.eu",
-    client_id: "10333f08-7e8f-4cc4-bba0-e66f4e5505d0",        
-    redirect_uri: "https://eosc-synergy.github.io/sqaaas-web/callback",
-    url_authorize: "https://aai-dev.egi.eu/oidc/authorize",
-    url_user_info: "https://aai-dev.egi.eu/oidc/userinfo",
-}
