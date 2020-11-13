2b5dccb4786adc3538fc0df75a50a2ae063ce288
dianamnaranjo92 <dianamariand92@gmail.com>
2020-11-13 13:54:47 +0100

    working on API integration

--- src/services/services.js
+++ src/services/services.js
@@ -1,10 +1,15 @@
 export default {
+  data(){
+    return {
+      api: 'http://194.210.120.204:20495/stage/v1/pipeline',
+    }
+  },
   methods: {
       createPipelineCall(data, callBackHandler) {
           console.log(data)
           axios({
               method: 'post',
-              url: 'http://194.210.120.204:20495/stage/v1/pipeline',
+              url: this.api,
               data: data
           }).then(response => {
               return callBackHandler(response);
@@ -16,7 +21,7 @@ export default {
       deletePipelineCall(data, callBackHandler) {
           axios({
               method: 'delete',
-              url: 'http://194.210.120.204:20495/stage/v1/pipeline/'+data,
+              url: this.api +'/'+data,
           }).then(response => {
               return callBackHandler(response);
           })
@@ -27,7 +32,7 @@ export default {
       runPipelineCall(data, callBackHandler){
         axios({
           method: 'post',
-          url: 'http://194.210.120.204:20495/stage/v1/pipeline/'+data+'/run',
+          url: this.api +'/'+data+'/run',
         }).then(response => {
             return callBackHandler(response);
         })
@@ -39,7 +44,7 @@ export default {
       checkStatusCall(data, callBackHandler){
         axios({
           method: 'get',
-          url: 'http://194.210.120.204:20495/stage/v1/pipeline/'+data+'/status',
+          url: this.api +'/'+data+'/status',
         }).then(response => {
             return callBackHandler(response);
         })
@@ -51,7 +56,7 @@ export default {
     downloadFileCall(id, callBackHandler){
       axios({
           method: 'get',
-          url: 'http://194.210.120.204:20495/stage/v1/pipeline/'+id+'/compressed_files',
+          url: this.api +'/'+id+'/compressed_files',
         }).then(response => {
           console.log(response)
             return callBackHandler(response);
@@ -64,7 +69,7 @@ export default {
     pullRequestCall(id,data, callBackHandler){
       axios({
           method: 'post',
-          url: 'http://194.210.120.204:20495/stage/v1/pipeline/'+id+'/pull_request',
+          url: this.api +'/'+id+'/pull_request',
           data: data.repo
         }).then(response => {
             return callBackHandler(response);
