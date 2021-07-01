cd71b7e3c67eebf6b3d9aaaf083c3119b97d92af
diana <dianamariand92@gmail.com>
2021-07-01 10:59:25 +0200

    change api url

--- src/env.js
+++ src/env.js
@@ -4,5 +4,5 @@ module.exports = {
     "redirect_uri": "https://eosc-synergy.github.io/sqaaas-web/callback",
     "url_authorize": "https://aai-dev.egi.eu/oidc/authorize",
     "url_user_info": "https://aai-dev.egi.eu/oidc/userinfo",
-    "api" : "https://api.sqaaas.eosc-synergy.eu/v1/pipeline",
+    "api" : "https://api.sqaaas.eosc-synergy.eu/v1",
 }
