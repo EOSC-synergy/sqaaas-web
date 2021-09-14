e1999366e64085bb0e89e97344482490d1153432
diana <dianamariand92@gmail.com>
2021-09-15 00:45:20 +0200

    working on tools args

--- src/main.js
+++ src/main.js
@@ -25,7 +25,8 @@ window.axios.interceptors.request.use(function (config) {
   // Do something before request is sent
   // const AUTH_TOKEN = document.getElementsByName("token")["0"].content;
   var session = JSON.parse(localStorage.getItem("session"))
-  const AUTH_TOKEN = session.user.access_token
+  // const AUTH_TOKEN = session.user.access_token
+  const AUTH_TOKEN = 'eyJraWQiOiJvaWRjIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI2NmNiYjA1MWRhZDRiNmMzZGFjOGUwY2FiMWQ5YzBhNGMzODhmZDRhNTkzYTczYmEwZjhjYmY2MTU2MTI2MmRiQGVnaS5ldSIsImF6cCI6IjEwMzMzZjA4LTdlOGYtNGNjNC1iYmEwLWU2NmY0ZTU1MDVkMCIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUiLCJpc3MiOiJodHRwczpcL1wvYWFpLWRldi5lZ2kuZXVcL29pZGNcLyIsImV4cCI6MTYzMTY1OTQxMiwiaWF0IjoxNjMxNTczMDEyLCJqdGkiOiI2NTBjODM0OC1iYjFiLTQzMWEtYWYzOC05OWUyZmM5MzRhMzMifQ.GKtArC3m4fj6WaAGNi1bq-sMGZasCgCm8drmL3cmlbd1LREMLkfEbLOBngxdKEOgREhyMu9O8COTWzR2DNRZHtAJiAvuqUyy2uSFHdglo9UqVPKJgk8-WUcfJ95z44OnF7Lq53-EIJHUIpqURo71yHorhnbUrfmcBYhm_CrUWqz-1slWae43Mb-XFOCujCd6M8CePfIP_NRYBlG-CLkYAZ7ti9spyanQ_CupCGg6TC-M_etp_H5xS4BIfsxjdI3pgkRhdePRqvsaZnJnrbaZOVwBwrO9QoZSfTqpXqMvn1I5CZr1GydLJ4gp2zkxeuLLO55O77xeZ_gpHs7poKlVqQ'
   if(AUTH_TOKEN){
       config.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`
   }
