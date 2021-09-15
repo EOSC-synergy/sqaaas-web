915c856a89971e95fb8971af0cb97674fcb9f5d3
dianamnaranjo92 <dianamariand92@gmail.com>
2021-09-15 10:29:53 +0200

    add token

--- src/main.js
+++ src/main.js
@@ -26,7 +26,8 @@ window.axios.interceptors.request.use(function (config) {
   // const AUTH_TOKEN = document.getElementsByName("token")["0"].content;
   var session = JSON.parse(localStorage.getItem("session"))
   // const AUTH_TOKEN = session.user.access_token
-  const AUTH_TOKEN = 'eyJraWQiOiJvaWRjIiwiYWxnIjoiUlMyNTYifQ.eyJzdWIiOiI2NmNiYjA1MWRhZDRiNmMzZGFjOGUwY2FiMWQ5YzBhNGMzODhmZDRhNTkzYTczYmEwZjhjYmY2MTU2MTI2MmRiQGVnaS5ldSIsImF6cCI6IjEwMzMzZjA4LTdlOGYtNGNjNC1iYmEwLWU2NmY0ZTU1MDVkMCIsInNjb3BlIjoib3BlbmlkIHByb2ZpbGUiLCJpc3MiOiJodHRwczpcL1wvYWFpLWRldi5lZ2kuZXVcL29pZGNcLyIsImV4cCI6MTYzMTc3NjMwMywiaWF0IjoxNjMxNjg5OTAzLCJqdGkiOiIzZjM4YzY1NS01MzMwLTQwMGItYTgzZS0zMDAxNzk3YmNjMmMifQ.dN4P4Z3uqkmD5FTVTBmFbX9zRk6rH7V4-dK_hs15BrE-xN-pPb4AHqCzjifQyINJX9UE4JYMDZW4L39DcMScFp4Dd5XTe6Ti1YgTJIrFKLZXoeFftq9_5cWDd7i2AeH9PCA2qUowNipizOpRu7LjbiU3eApX52G8yPjtv0H7vJEndgyzwWwt5wKC8_DWgaFPJUc_UA9XDAEvZNRH9ADvuc6jkz9W5ax-bvYAgKUUnj_lGlpe-yFoQypFY2ecT9MfT3FxafOasg5q3_F34LFtKrQEYRMKdU1iOyFEfvGQb9NK-OBn82I6rIABcluCt5vWjmqXeeqxxvhYJx5sk6vQTA'
+  const AUTH_TOKEN = this.env.token
+
   if(AUTH_TOKEN){
       config.headers.common['Authorization'] = `Bearer ${AUTH_TOKEN}`
   }
